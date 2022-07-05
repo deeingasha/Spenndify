3333a18cf69be258b2694edbfa6b1381579e5b19
Dorcas <dorcasingasha@gmail.com>
2022-07-05 11:56:30 +0300

    Fixed issues

--- app/src/main/java/com/example/spenndify/createaccount/CreateAccountFragment.kt
+++ app/src/main/java/com/example/spenndify/createaccount/CreateAccountFragment.kt
@@ -12,6 +12,12 @@ import com.example.spenndify.databinding.AccCreationBinding
 class CreateAccountFragment: Fragment() {
     private lateinit var binding: AccCreationBinding
 
+   /** val firstName = (binding.fName.text).toString()
+    val lastName = binding.lName.text.toString()
+    val idNo = binding.idNo.text.toString()
+    val email = binding.email.text.toString()*/
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -27,34 +33,10 @@ class CreateAccountFragment: Fragment() {
 
 
         binding.continueCaBtn.setOnClickListener {
-
-            val firstName = (binding.fName.text).toString()
-            val lastName = binding.lName.text.toString()
-            val idNo = binding.idNo.text.toString()
-            val email = binding.email.text.toString()
-
-           if (firstName.length == 0){
-               binding.fnameInputLayout.isErrorEnabled = true
-                binding.fnameInputLayout.error = "Please input first name"
-            }else if (lastName.length == 0){
-                binding.lnameInputLayout.isErrorEnabled = true
-                binding.lnameInputLayout.error = "Please input last name"
-            }else if (idNo.length == 0){
-                binding.idnoInputLayout.isErrorEnabled = true
-                binding.idnoInputLayout.error = "Please input ID number"
-            }else if (idNo.length <8){
-                binding.idnoInputLayout.isErrorEnabled = true
-                binding.idnoInputLayout.error = "ID number should be 8 characters"
-           }else if (email.length == 0){
-               binding.emailInputLayout.isErrorEnabled = true
-               binding.emailInputLayout.error= "Please input email address"
-           }
-           else {
-               val action =
-                   CreateAccountFragmentDirections.actionCreateAccountFragmentToSecurityQuestionFragment(firstName,lastName)
-               findNavController().navigate(action)
-           }
-
+           // validateForm()
+            val action =
+                CreateAccountFragmentDirections.actionCreateAccountFragmentToSecurityQuestionFragment(fname = "", lname = "")
+            findNavController().navigate(action)
         }
 
         binding.backCaBtn.setOnClickListener {
@@ -62,6 +44,31 @@ class CreateAccountFragment: Fragment() {
                 findNavController().navigate(action)
         }
 
-
     }
+  /**  fun validateForm(){
+
+        if (firstName.length == 0){
+            binding.fnameInputLayout.isErrorEnabled = true
+            binding.fnameInputLayout.error = "Please input first name"
+        }else if (lastName.length == 0){
+            binding.lnameInputLayout.isErrorEnabled = true
+            binding.lnameInputLayout.error = "Please input last name"
+        }else if (idNo.length == 0){
+            binding.idnoInputLayout.isErrorEnabled = true
+            binding.idnoInputLayout.error = "Please input ID number"
+        }else if (idNo.length <8){
+            binding.idnoInputLayout.isErrorEnabled = true
+            binding.idnoInputLayout.error = "ID number should be 8 characters"
+        }else if (email.length == 0){
+            binding.emailInputLayout.isErrorEnabled = true
+            binding.emailInputLayout.error= "Please input email address"
+        }else if(email.length > 0){
+
+        }
+        else {
+            val action =
+                CreateAccountFragmentDirections.actionCreateAccountFragmentToSecurityQuestionFragment(firstName,lastName)
+            findNavController().navigate(action)
+        }
+    }*/
 }
